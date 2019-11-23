cd5cff4d131ed1e79528a687b06e978f572371e6
Diego <dlandrade2103@gmail.com>
2019-11-23 13:47:13 +0000

    added test for views

--- postcodes_app/tests.py
+++ postcodes_app/tests.py
@@ -1,3 +1,36 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
 
-# Create your tests here.
+from .views import index, check
+
+
+class IndexTests(SimpleTestCase):
+
+    def setUp(self):
+        url = reverse('index')
+        self.response = self.client.get(url)
+
+    def test_index_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_index_template(self):
+        self.assertTemplateUsed(self. response, 'validate_postcode.html')
+
+    def test_index_contains_correct_html(self):
+        self.assertContains(self.response, 'UK Postcode Validator')
+
+    def test_index_does_not_contain_incorrect_html(self):
+        self.assertNotContains(self.response, 'Hi there! T should not be on the page.')
+
+
+class CheckTests(SimpleTestCase):
+
+    def setUp(self):
+        url = reverse('check')
+        self.response = self.client.get(url)
+
+    def test_check_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_check_template(self):
+        self.assertTemplateUsed(self.response, 'validate_postcode.html')
